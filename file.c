5ab324ab304350d008a6e2a00730bfb4219d7318
Yasmine Karray <ykarray29@gmail.com>
2023-04-26 21:21:30 +0100

    Update file.c
    Ce code en langage C implémente une structure de données de file d'attente (FIFO : Premier Entré, Premier Sorti) à l'aide d'une liste chaînée. Le code fournit des fonctions pour initialiser la file d'attente, ajouter des éléments à la fin de la file d'attente, supprimer des éléments de la tête de la file d'attente et afficher le contenu de la file d'attente. La fonction principale initialise une file d'attente, ajoute plusieurs éléments, affiche la file d'attente, la vide en retirant les éléments un par un et affiche à nouveau la file d'attente vide. Le code est bien commenté pour faciliter la compréhension de chaque fonction et son rôle dans la mise en œuvre de la file d'attente.

--- file.c
+++ file.c
@@ -77,29 +77,99 @@ e=e->next;
 
 
 
+}// File FIFO (First In First Out)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure pour représenter un élément de la file
+typedef struct Element {
+    int val;
+    struct Element *next;
+} Element;
+
+// Structure pour représenter la file elle-même
+typedef struct {
+    Element *tete; // Pointeur vers le premier élément de la file
+    Element *queue; // Pointeur vers le dernier élément de la file
+} file;
+
+// Fonction pour initialiser une file vide
+void init(file *f) {
+    f->tete = NULL;
+    f->queue = NULL;
 }
 
-void main()
-{
-file f;
-init(&f);
-printf("vide : %d \n",vide(f));
-enfiler(&f,1);
+// Fonction pour vérifier si une file est vide
+int vide(file f) {
+    return f.tete == NULL;
+}
 
-enfiler(&f,2);
-enfiler(&f,3);
-enfiler(&f,4);
-enfiler(&f,5);
-enfiler(&f,6);
-printf("afficher file aprés remplissage\n");
-afficher(f);
-printf("\n vide : %d \n",vide(f));
-printf("\n tete : %d \n",tete(f));
-printf("\nqueue : %d \n",queue(f));
-printf("valeur defilés : %d \n", defiler(&f));
-printf("afficher file aprés avoir défilé (first in first out)\n");
-afficher(f);
-printf("\n");
+// Fonction pour retourner le contenu de la tête de la file
+int tete(file f) {
+    return (f.tete)->val;
+}
+
+// Fonction pour retourner le contenu de la queue de la file
+int queue(file f) {
+    return (f.queue)->val;
+}
+
+// Fonction pour enfiler un élément dans la file
+void enfiler(file *f, int x) {
+    Element *p = (Element*) malloc(sizeof(Element)); // Allouer un nouvel élément
+    p->val = x; // Initialiser sa valeur
+    p->next = NULL;
+    if (vide(*f) == 1) { // Si la file est vide, le nouvel élément devient à la fois la tête et la queue
+        f->tete = p;
+        f->queue = p;
+    } else { // Sinon, ajouter le nouvel élément à la fin de la file
+        f->queue->next = p;
+        f->queue = p;
+    }
+}
+
+// Fonction pour défiler un élément de la file
+int defiler(file *f) {
+    int x = f->tete->val; // Récupérer la valeur de l'élément à défiler
+    Element *p = f->tete; // Stocker l'adresse de l'élément à supprimer
+    f->tete = f->tete->next; // Mettre à jour la tête de la file
+    free(p); // Libérer la mémoire allouée à l'élément à supprimer
+    if (f->tete == NULL) { // Si la file est maintenant vide, mettre à jour la queue
+        f->queue = NULL;
+    }
+    return x;
+}
+
+// Fonction pour afficher le contenu de la file
+void afficher(file f) {
+    Element *e = f.tete;
+    while (e != NULL) {
+        printf("%d \t", e->val);
+        e = e->next;
+    }
+}
+
+// Fonction principale pour tester les fonctions de la file
+void main() {
+    file f;
+    init(&f);
+    printf("vide : %d \n", vide(f));
+    enfiler(&f, 1);
+    enfiler(&f, 2);
+    enfiler(&f, 3);
+    enfiler(&f, 4);
+    enfiler(&f, 5);
+    enfiler(&f, 6);
+    printf("afficher file après remplissage\n");
+    afficher(f);
+    printf("\n vide : %d \n", vide(f));
+    printf("\n tete : %d \n", tete(f));
+    printf("\n queue : %d \n", queue(f));
+    printf("valeur defilés : %d \n", defiler(&f));
+    printf("afficher file aprés avoir défilé (first in first out)\n");
+    afficher(f);
+    printf("\n");
 }
 
 
